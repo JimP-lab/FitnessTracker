26952789e6d17ba27e1543466ddaed184bcb8f1b
Jim Paraschakis <140411754+JimP-lab@users.noreply.github.com>
2024-11-12 22:42:03 +0200

    Update Save_metrics.php

--- Databases/Save_metrics.php
+++ Databases/Save_metrics.php
@@ -1,8 +1,8 @@
 <?php
 $servername = 'localhost';
-$username = 'your_username';
-$password = 'your_password';
-$dbname = 'your_database';
+$username = 'fit';
+$password = 'root';
+$dbname = '';
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
