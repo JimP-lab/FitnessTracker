9a09f97f850e44898029de53dacf8aad1151eaab
Jim Paraschakis <140411754+JimP-lab@users.noreply.github.com>
2024-11-12 22:07:30 +0200

    Add files via upload

---
+++ Databases/ChangePassword.php
@@ -0,0 +1,46 @@
+<?php
+// Database credentials
+$host = 'localhost';
+$dbname = 'u879781544_Fit_Tracker';
+$username = 'u879781544_Fit';
+$password = '8??60eZs';
+
+// Get the posted data
+$data = json_decode(file_get_contents('php://input'), true);
+$user = $data['username'];
+$newPassword = $data['password'];
+
+// Validate the inputs (e.g., check length, complexity, etc.)
+if (strlen($newPassword) < 10) {
+    echo json_encode(['status' => 'error', 'message' => 'Password Must Be At Least 10 Characters Long.']);
+    exit;
+}
+
+// Create connection
+$mysqli = new mysqli($host, $username, $password, $dbname);
+
+// Check connection
+if ($mysqli->connect_errno) {
+    echo json_encode(['status' => 'error', 'message' => 'Connection Error: ' . $mysqli->connect_error]);
+    exit();
+}
+
+// Hash the new password
+$hashedPassword = password_hash($newPassword, PASSWORD_BCRYPT);
+
+// Update the password in the database
+$sql = "UPDATE users SET password = ? WHERE username = ?";
+$stmt = $mysqli->prepare($sql);
+$stmt->bind_param("ss", $hashedPassword, $user);
+if ($stmt->execute()) {
+    // Log the user in by starting a session
+    session_start();
+    $_SESSION['user_id'] = $user; // Assuming 'user_id' is the user's username
+    echo json_encode(['status' => 'success', 'message' => 'Password Was Updated']);
+} else {
+    echo json_encode(['status' => 'error', 'message' => 'Failed To Update']);
+}
+
+$stmt->close();
+$mysqli->close();
+?>
---
+++ Databases/CheckUserExists.php
@@ -0,0 +1,40 @@
+<?php
+// Decode incoming JSON data
+$data = json_decode(file_get_contents('php://input'), true);
+$email = $data['email'];
+$username = $data['username'];
+
+// Database credentials
+$host = 'localhost';
+$dbname = 'u879781544_Fit_Tracker';
+$dbUsername = 'u879781544_Fit';
+$dbPassword = '8??60eZs';
+
+// Connect to the database
+$mysqli = new mysqli($host, $dbUsername, $dbPassword, $dbname);
+
+// Check for a connection error
+if ($mysqli->connect_errno) {
+    die("Connection error: " . $mysqli->connect_error);
+}
+
+// Prepare the SQL statement to check if a user with the same email or username exists
+$sql = "SELECT * FROM users WHERE email = ? OR username = ?";
+$stmt = $mysqli->prepare($sql);
+$stmt->bind_param("ss", $email, $username);
+$stmt->execute();
+$stmt->store_result();
+
+// Check if any record was found
+if ($stmt->num_rows > 0) {
+    // If a user with the same email or username exists
+    echo "User Exists";
+} else {
+    // If no user is found
+    echo "User Does Not Exist";
+}
+
+// Close the statement and database connection
+$stmt->close();
+$mysqli->close();
+?>
