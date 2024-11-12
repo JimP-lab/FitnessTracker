70d5cb73446d51ac8d7cfb9c4e293d42942ed4f6
Jim Paraschakis <140411754+JimP-lab@users.noreply.github.com>
2024-11-12 22:35:50 +0200

    Update CheckCredentials.php

--- Databases/CheckCredentials.php
+++ Databases/CheckCredentials.php
@@ -3,6 +3,7 @@ header('Content-Type: application/json');
 $data = json_decode(file_get_contents('php://input'), true);
 $username = $data['username'];
 $password = $data['password'];
+
 // Database credentials
 $host = 'localhost';
 $dbname = 'your_database';
