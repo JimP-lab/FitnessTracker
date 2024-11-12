750af727d2aefed582b3398fefc9999175265501
Jim Paraschakis <140411754+JimP-lab@users.noreply.github.com>
2024-11-12 22:03:24 +0200

    Update Mailer.php

--- phpmailer/Mailer.php
+++ phpmailer/Mailer.php
@@ -13,7 +13,7 @@ try {
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
     $mail->Username = 'sfitnesstracker116@gmail.com';
-    $mail->Password = 'whff cbnq qsbs veod'; // Consider using environment variables for security
+    $mail->Password = 'whff cbnq qsbs veod'; 
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
     $mail->Port = 587;
 } catch (Exception $e) {
